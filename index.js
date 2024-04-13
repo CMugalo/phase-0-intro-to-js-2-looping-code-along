da8bc443f8f15901a3a30f7a315bc5ce8395bef7
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-13 17:51:44 +0300

    adds for and while loop

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 // Code your solutions in this file
 
-const recipients = ["Guadalupe", "Ollie", "Aki"];
-const copyOfRecipients = [...recipients];
+const recipientNames = ["Guadalupe", "Ollie", "Aki"];
 
-function writeCards(copyOfRecipients, event) {
-  for (let i = 0; i < copyOfRecipients.length; i++) {
-    const recipients = ["Guadalupe", "Ollie", "Aki"];
-    const copyOfRecipients = [...recipients];
-    const messageForRecipients = [
-      `Thank you, ${copyOfRecipients[i]}, for the wonderful ${event} gift!`,
-      `Thank you, ${copyOfRecipients[1]}, for the wonderful ${event} gift!`,
-      `Thank you, ${copyOfRecipients[2]}, for the wonderful ${event} gift!`,
-    ];
+function writeCards(stringArray, eventName) {
+  const messagesArray = [];
 
-    return messageForRecipients;
+  for (let i = 0; i < stringArray.length; i++) {
+    messagesArray.push(
+      `Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`
+    );
   }
+  return messagesArray;
 }
 
-//
+writeCards(recipientNames, "surprise");
 
-function countDown(callCount) {
-  // let callCount = true;
-  {
-    let i = 11;
-    while (i > 0) {
-      console.log(callCount[i]);
-      i--;
-      //numberCount = console.log(callCount[i]);
-    }
-    return callCount;
+function countDown(posInteger) {
+  let i = posInteger;
+  while (i >= 0) {
+    console.log(i);
+    i--;
   }
+  return posInteger;
 }
-
-countDown();
+countDown(3);
